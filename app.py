7e29afc453b5d2ac2803d74a6c5fbb148ab47f90
Ansh-1306 <ansh-mdtech@gmail.com>
2024-04-05 02:37:05 +0530

    changes

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-    return render_template('index.html')
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+
+@app.route("/login")
+def login_page():
+    return render_template('login.html')
 
     
