aea17a95eef82972a973b468163bb81efff06960
Ansh-1306 <ansh-mdtech@gmail.com>
2024-04-05 23:56:43 +0530

    xampp added

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 from flask import Flask, render_template
+import mysql.connector
+from models import Class
 
+
+        
 app = Flask(__name__)
+
+connection = mysql.connector.connect(
+host="localhost",
+user="root",
+password="",
+database="fitness"
+)
+
+cursor = connection.cursor()
+
+def load_classes_from_db():    
+    try:
+        cursor.execute("select * from classes")
+        list_classes=cursor.fetchall()
+        connection.commit()
+        
+        classes=[]
+
+        for c in list_classes:
+            cl = Class(c[0],c[1],c[2],c[3],c[4],c[5],c[6])
+            classes.append(cl)
+
+        return classes        
+    except mysql.connector.Error as e:
+        print(e)
+    
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return render_template('home.html')
+    classes = load_classes_from_db()
+
+    return render_template('home.html', classes = classes)
 
 
 @app.route("/login")
---
+++ models.py
@@ -0,0 +1,10 @@
+class Class:
+    def __init__(self,id,name,img,icon,des,total,booked):
+        self.id=id
+        self.name=name
+        self.img=img
+        self.icon=icon
+        self.des=des
+        self.total=total
+        self.booked=booked
+        
