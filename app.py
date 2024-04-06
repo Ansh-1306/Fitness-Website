9bd90bfec63d475b39b12845c16214b3986953d4
Ansh-1306 <ansh-mdtech@gmail.com>
2024-04-06 08:26:56 +0530

    final

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 from models import Class
 
@@ -13,10 +13,13 @@ password="",
 database="fitness"
 )
 
-cursor = connection.cursor()
+
+global isLoggedIn 
+isLoggedIn = False
 
 def load_classes_from_db():    
     try:
+        cursor = connection.cursor()    
         cursor.execute("select * from classes")
         list_classes=cursor.fetchall()
         connection.commit()
@@ -30,18 +33,96 @@ def load_classes_from_db():
         return classes        
     except mysql.connector.Error as e:
         print(e)
-    
+    finally:
+        cursor.close()
+
 
 @app.route("/")
 @app.route("/home")
 def home_page():
     classes = load_classes_from_db()
-
-    return render_template('home.html', classes = classes, login=False)
+    global isLoggedIn
+    isLoggedIn = False
+    return render_template('home.html', classes = classes, login=False, isLoggedIn = isLoggedIn)
 
 
 @app.route("/login")
 def login_page():
-    return render_template('login.html', login=True)
+    global isLoggedIn
+    isLoggedIn = False
+    return render_template('login.html', login=True, isLoggedIn = False)
+
+@app.route("/sign_up", methods=['POST','GET'])
+def sign_up():
+    try:
+        global isLoggedIn
+        cursor = connection.cursor()   
+        name = request.form['name']
+        phone = request.form['phone']
+        password = request.form['password']
+        cpassword = request.form['cpassword']
+        age = request.form['age']
+        gender = request.form['gender']
+        height = request.form['height']
+        weight = request.form['weight']
+        cursor.execute('select * from users')
+        list_users = cursor.fetchall()
+        for user in list_users:
+            if user[2] == phone:
+                msg =  "User already exists!"
+                isLoggedIn = False
+                return render_template('login.html', isLoggedIn = isLoggedIn, msg=msg )
+        if cpassword == password:
+            insert_query = "INSERT INTO users(name, phone, age, height, weight, gender, password) VALUES ('"+name+"','"+phone+"',"+age+","+height+","+weight+",'"+gender+"','"+password+"')"
+            cursor.execute(insert_query)
+            connection.commit()
+            if(cursor.rowcount>0):
+                msg = "Account Created Successfully" 
+                isLoggedIn = True
+                classes = load_classes_from_db()
+                return render_template('home.html', classes = classes, isLoggedIn = isLoggedIn)
+            else:
+                msg = "There was an error during sign up. Please Try Again!"
+        else:
+            msg = "Password does not match"
+            isLoggedIn = False
+        return render_template('login.html', isLoggedIn = isLoggedIn, msg = msg)
+    except mysql.connector.Error as e:
+        print(e)
+    finally:
+        cursor.close()
+    
+
+
+@app.route("/sign-in", methods=['POST','GET'])
+def login():
+    try:
+        global isLoggedIn
+        cursor = connection.cursor()   
+        phone = request.form['phone']
+        password = request.form['password']
+    
+        cursor.execute('select * from users')
+        list_users = cursor.fetchall()
+        for user in list_users:
+            if user[2] == phone:
+                if user[7] == password:
+                    msg = "Account Created Successfully"
+                    isLoggedIn = True
+                    classes = load_classes_from_db()
+                    return render_template('home.html', classes = classes, login=False, isLoggedIn = isLoggedIn)
+                else:
+                    msg = "Incorrect Password"
+            else:
+                msg = "User does not exist. Please Sign Up first "
+
+        connection.commit()
+        isLoggedIn = False
+        return render_template('login.html', login=True, isLoggedIn = isLoggedIn, msg = msg)
+    except mysql.connector.Error as e:
+        print(e)
+    finally:
+        cursor.close()
+
 
     
--- models.py
+++ models.py
@@ -7,4 +7,15 @@ class Class:
         self.des=des
         self.total=total
         self.booked=booked
+
+class User:
+    def __init__(self,id,name,phone,age,height,weight,gender,password):
+        self.id=id
+        self.name=name
+        self.phone=phone
+        self.age=age
+        self.height=height
+        self.weight=weight
+        self.gender=gender
+        self.password=password
         
